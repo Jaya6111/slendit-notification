b7f6eb8b38ed5cc735f586f0287ce3576aa390ea
Jaya6111 <136075867+Jaya6111@users.noreply.github.com>
2024-12-07 10:27:02 +0530

    Uploading Slendit mail
    uploading the slendit notification service

---
+++ slendit-mail/src/main/java/com/fq/slendit/mail/SlenditMailApplication.java
@@ -0,0 +1,13 @@
+package com.fq.slendit.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlenditMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlenditMailApplication.class, args);
+	}
+
+}
---
+++ slendit-mail/src/test/java/com/fq/slendit/mail/SlenditMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fq.slendit.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlenditMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
